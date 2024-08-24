4c4bb92850e85638d1ee5f5824b471da257697c1
PrashantBtkl <prashant44backup@gmail.com>
2024-08-24 14:21:50 +0530

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<div> dev tools like, json formatter, lint, beautify and validate
+<br><br>
+<a href="/json-formatter">click here</a> to go to json editor 
+ </div>
+
+<style>
+div, a {
+font-family: arial;
+}
+
+</style>
+
+
